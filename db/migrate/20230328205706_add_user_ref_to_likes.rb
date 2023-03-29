01e52cf24c269a9eef98942c9ab839ea2a7d78af
rmrmrmr <ricardomartineza77@gmail.com>
2023-03-28 20:47:24 -0600

    Change author_id to user_id on Likes & Comments tables

--- db/migrate/20230328205706_add_user_ref_to_likes.rb
+++ db/migrate/20230328205706_add_user_ref_to_likes.rb
@@ -1,5 +1,5 @@
 class AddUserRefToLikes < ActiveRecord::Migration[7.0]
   def change
-    add_reference :likes, :auhor, null: false, foreign_key: { to_table: :users }, index: { unique: true, name: 'user_like_index'}
+    add_reference :likes, :user, null: false, foreign_key: true, index: { unique: true, name: 'user_like_index'}
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,19 +18,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_210402) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
+    t.bigint "user_id", null: false
     t.bigint "post_id", null: false
-    t.index ["author_id"], name: "user_comment_index", unique: true
     t.index ["post_id"], name: "post_comment_index", unique: true
+    t.index ["user_id"], name: "user_comment_index", unique: true
   end
 
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "auhor_id", null: false
+    t.bigint "user_id", null: false
     t.bigint "post_id", null: false
-    t.index ["auhor_id"], name: "user_like_index", unique: true
     t.index ["post_id"], name: "post_like_index", unique: true
+    t.index ["user_id"], name: "user_like_index", unique: true
   end
 
   create_table "posts", force: :cascade do |t|
@@ -54,8 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_210402) do
   end
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users", column: "auhor_id"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users", column: "author_id"
 end
