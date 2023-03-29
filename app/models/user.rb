b565da44cd9781afb04381d58b07e6d5f2b0bbf6
rmrmrmr <ricardomartineza77@gmail.com>
2023-03-28 23:20:57 -0600

    Create custom methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+
+  def latest_posts
+    posts.order(created_at: :desc).limit(3).reverse
+  end
 end
