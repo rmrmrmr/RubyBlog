cba6630879da76608a865613d83514e8ccf94cba
rmrmrmr <ricardomartineza77@gmail.com>
2023-03-29 18:43:22 -0600

    Add unit tests for User validations

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { User.new(name: 'Richard', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Proud dad and rockstar developer') }
+
+  before { subject.save }
+
+  # validation tests
+  it 'validates the presence of name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'validates that posts_counter was set' do
+    expect(subject).to be_valid
+    expect(subject.posts_counter).to eql(0)
+  end
+
+  it 'validates the presence of posts_counter' do
+    subject.posts_counter = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'validates that posts counter is a positive integer' do
+    subject.posts_counter = -1
+    expect(subject).to_not be_valid
+  end
+end
