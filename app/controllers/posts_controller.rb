0b5865403f7a83f8f87ccc03b4122589178f00cb
rmrmrmr <ricardomartineza77@gmail.com>
2023-04-07 14:53:48 -0600

    Create a form to create a post on behalf of the first user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,28 @@ class PostsController < ApplicationController
     @posts = @user.posts
   end
 
+  def new
+    @post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: @post} }
+    end
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :text))
+    @post.author = current_user
+    respond_to do |format|
+      format.html do
+        if @post.save
+          redirect_to user_path(@current_user)
+        else
+          flash.now[:error] = "Error: Post could not be saved :("
+          render :new, locals: { post: @post }
+        end
+      end
+    end
+  end  
+
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
