35700493e0448bcba0bc49bacb56a4f0484675be
Juank628 <jsanchez_zunino@yahoo.com>
2023-04-10 16:53:53 -0500

    Create user show page tests

---
+++ spec/views/user_show_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'User show', type: :feature do
+    scenario 'show user picture' do
+        visit user_path(1)
+        expect(page).to have_selector('img[src="https://unsplash.com/photos/F_-0BxGuVvo"]')
+    end
+
+    scenario 'show username' do
+        visit user_path(1)
+        expect(page).to have_content('Tom')
+    end
+
+    scenario 'show number of posts' do
+        visit user_path(1)
+        expect(page).to have_content('Number of posts: 4')
+    end
+
+    scenario 'show user bio' do
+        visit user_path(1)
+        expect(page).to have_content('Teacher from Mexico.')
+    end
+
+    scenario 'show last 3 user posts' do
+        visit user_path(1)
+        expect(page).to have_selector('.post-identifier', :count => 3)
+    end
+
+    scenario 'show view all posts button' do
+        visit user_path(1)
+        expect(page).to have_selector('a', :text => 'See all posts')
+    end
+
+    scenario 'clicks on users post redirect to post show page' do
+        visit user_path(1)
+        click_link(href: '/users/1/posts/2')
+        expect(page).to have_current_path('/users/1/posts/2')  
+    end
+
+    scenario 'clicks on sell all posts redirect to posts index page' do
+        visit user_path(1)
+        click_link(href: '/users/1/posts')
+        expect(page).to have_current_path('/users/1/posts')  
+    end
+end
