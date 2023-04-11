bd46a629b5a779d957c9b3fb8e3d9e1dc67c60f4
rmrmrmr <ricardomartineza77@gmail.com>
2023-04-10 18:39:39 -0600

    Fix n+1 problem

--- spec/views/post_show_spec.rb
+++ spec/views/post_show_spec.rb
@@ -1,44 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe 'User show', type: :feature do
-    scenario 'show user picture' do
-        visit user_post_path(user_id: 1, id: 1)
-        expect(page).to have_selector('h1', :text => 'Hello')
-    end
+  scenario 'show user picture' do
+    visit user_post_path(user_id: 1, id: 1)
+    expect(page).to have_selector('h1', text: 'Hello')
+  end
 
-    scenario 'show user picture' do
-        visit user_post_path(user_id: 1, id: 1)
-        expect(page).to have_selector('h1', :text => 'Tom')
-    end
+  scenario 'show user picture' do
+    visit user_post_path(user_id: 1, id: 1)
+    expect(page).to have_selector('h1', text: 'Tom')
+  end
 
-    scenario "Show the number of comments for a post" do
-        visit user_post_path(user_id: 1, id: 1)
-        expect(page).to have_content("Comments: 6")      
-    end
+  scenario 'Show the number of comments for a post' do
+    visit user_post_path(user_id: 1, id: 1)
+    expect(page).to have_content('Comments: 6')
+  end
 
-    scenario "Show the number of likes for a post" do
-        visit user_post_path(user_id: 1, id: 1)
-        expect(page).to have_content("Likes: 3")      
-    end
+  scenario 'Show the number of likes for a post' do
+    visit user_post_path(user_id: 1, id: 1)
+    expect(page).to have_content('Likes: 3')
+  end
 
-    scenario "Show the content of a post" do
-        visit user_post_path(user_id: 1, id: 1)
-        expect(page).to have_selector('h2', :text => 'This is my first post')  
-    end
+  scenario 'Show the content of a post' do
+    visit user_post_path(user_id: 1, id: 1)
+    expect(page).to have_selector('h2', text: 'This is my first post')
+  end
 
-    scenario "Show the username of all commenters" do
-        visit user_post_path(user_id: 1, id: 1)
-        expect(page).to have_selector('.commenter-identifier', :text => 'Tom', :count => 3)  
-        expect(page).to have_selector('.commenter-identifier', :text => 'Lilly', :count => 2)  
-    end
+  scenario 'Show the username of all commenters' do
+    visit user_post_path(user_id: 1, id: 1)
+    expect(page).to have_selector('.commenter-identifier', text: 'Tom', count: 3)
+    expect(page).to have_selector('.commenter-identifier', text: 'Lilly', count: 2)
+  end
 
-    scenario "Show the comment of all commenter" do
-        visit user_post_path(user_id: 1, id: 1)
-        expect(page).to have_selector('.comment-identifier', :text => 'Tom' && 'Second comment')  
-        expect(page).to have_selector('.comment-identifier', :text => 'Tom' && 'Fourth comment')  
-        expect(page).to have_selector('.comment-identifier', :text => 'Tom' && 'Sixth comment') 
-        
-        expect(page).to have_selector('.comment-identifier', :text => 'Lilly' && 'Third comment')  
-        expect(page).to have_selector('.comment-identifier', :text => 'Lilly' && 'Fifth comment')  
-    end
+  scenario 'Show the comment of all commenter' do
+    visit user_post_path(user_id: 1, id: 1)
+    expect(page).to have_selector('.comment-identifier', text: 'Tom' && 'Second comment')
+    expect(page).to have_selector('.comment-identifier', text: 'Tom' && 'Fourth comment')
+    expect(page).to have_selector('.comment-identifier', text: 'Tom' && 'Sixth comment')
+
+    expect(page).to have_selector('.comment-identifier', text: 'Lilly' && 'Third comment')
+    expect(page).to have_selector('.comment-identifier', text: 'Lilly' && 'Fifth comment')
+  end
 end
--- spec/views/posts_index_spec.rb
+++ spec/views/posts_index_spec.rb
@@ -13,44 +13,42 @@ RSpec.describe 'Posts Index', type: :feature do
 
   scenario 'Shows the number of posts the user has written' do
     visit user_posts_path(1)
-    expect(page).to have_content('Number of posts: 4')  
+    expect(page).to have_content('Number of posts: 4')
   end
-  
+
   scenario "Shows a post's title" do
     visit user_posts_path(1)
     expect(page).to have_selector('.title-identifier')
   end
 
-  scenario "Show the content of a post" do
+  scenario 'Show the content of a post' do
     visit user_posts_path(1)
-    expect(page).to have_content('This is my first post')  
+    expect(page).to have_content('This is my first post')
   end
 
-  scenario "Show some of the comments for each post" do
+  scenario 'Show some of the comments for each post' do
     visit user_posts_path(1)
-    expect(page).to have_selector('.comment-identifier')  
+    expect(page).to have_selector('.comment-identifier')
   end
 
-  scenario "Show the number of comments for a post" do
+  scenario 'Show the number of comments for a post' do
     visit user_posts_path(1)
-    expect(page).to have_content("Comments: 6")      
+    expect(page).to have_content('Comments: 6')
   end
 
-  scenario "Show number of likes for a post" do
+  scenario 'Show number of likes for a post' do
     visit user_posts_path(1)
-    expect(page).to have_content("Likes: 3")  
+    expect(page).to have_content('Likes: 3')
   end
 
-  scenario "Show pagination if there are more posts that fit on the view" do
+  scenario 'Show pagination if there are more posts that fit on the view' do
     visit user_posts_path(1)
-    if page.all('.post-identifier').count > 3
-      expect(page).to have_content("Pagination")  
-    end
+    expect(page).to have_content('Pagination') if page.all('.post-identifier').count > 3
   end
 
   scenario "When a post is clicked, it redirects the user to that post's show page" do
     visit user_posts_path(1)
     click_link(href: '/users/1/posts/1')
-    expect(page).to have_current_path('/users/1/posts/1')  
+    expect(page).to have_current_path('/users/1/posts/1')
   end
 end
