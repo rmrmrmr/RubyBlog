d972258038c8a6dce233c04c9227476c1bf17781
rmrmrmr <ricardomartineza77@gmail.com>
2023-04-12 16:15:16 -0600

    Fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,9 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  Roles = [ :admin, :default ]
+  # rubocop:disable Naming/ConstantName
+  Roles = [:admin, :default]
+  # rubocop:enable Naming/ConstantName
 
   def admin?
     self.role == 'admin'
