d136b1bc03c37f07da9569861b6e3465368fe132
Juank628 <jsanchez_zunino@yahoo.com>
2023-04-13 16:45:56 -0500

    Setup authorization route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      # post 'login', to: 'login#auth'
+      post 'login', to: 'login#auth'
       resources :users, only: [:index, :show] do
         resources :posts, only: [:index] do
           resources :comments, only: [:index, :create]
