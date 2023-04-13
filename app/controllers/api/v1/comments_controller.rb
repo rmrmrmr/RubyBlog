ec3f6444090527c7c534d5c3ac5de22870d102b4
Juank628 <jsanchez_zunino@yahoo.com>
2023-04-13 16:46:59 -0500

    Create comments_controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::CommentsController < ApiController
+  before_action :authorize_request, only: [:create]
+
   def index
     # /api/v1/users/:user_id/posts/:post_id/comments(.:format)
     # @user = User.includes(posts: [comments: [:user]]).find(params[:user_id])
@@ -6,5 +8,11 @@ class Api::V1::CommentsController < ApiController
     @post = Post.find(params[:post_id])
 
     render json: @post.comments
+  end
+
+  def create
+    @text = params[:text]
+    @new_comment = Comment.create(text: @text, user_id: @current_user.id, post_id: params[:post_id])
+    render json: @new_comment
   end
 end
